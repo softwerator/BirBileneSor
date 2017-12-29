5794a878436a5d144397ea6a5211c8750fa66744
AYDIN BULUT <ab@aydinbulut.com>
2017-12-29 23:43:37 +0300

    Create User Request

---
+++ app/Http/Requests/StoreUser.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUser extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'     => 'required|string',
+            'surname'  => 'required|string',
+            'email'    => 'required|unique:users',
+            'password' => 'required|string',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'name'     => 'Ad',
+            'surname'  => 'Soyad',
+            'email'    => 'Eposta',
+            'password' => 'Şifre',
+        ];
+    }
+}
