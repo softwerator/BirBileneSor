4a93516d07cc72c5e62bf51259da3c206d66214a
AYDIN BULUT <ab@aydinbulut.com>
2017-12-30 05:21:27 +0300

    Update user model

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public static function getByToken($token)
+    {
+        return (new static)->where('login_token', $token)->first();
+    }
 }
