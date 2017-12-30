fbaae70715487aad59bec360fcb39af165870207
AYDIN BULUT <ab@aydinbulut.com>
2017-12-30 03:55:30 +0300

    Create Auth controllers

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Login;
+use App\User;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     */
+    public function login(Login $request)
+    {
+        // fetch user
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['status' => false, 'message' => __('auth.failed')]);
+        }
+
+        // get new token
+        $token = get_new_token();
+
+        $user = Auth::user();
+
+        // set login token
+        $user->login_token = $token;
+
+        // update user
+        $user->save();
+
+        return response()->json(['status' => true, 'token' => $token]);
+
+    }
+
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        if (!$request->has('token')) {
+            return response()->json(["status" => false, "message" => "Token ile eşleşen kullanıcı sistemde kayıtlı değil."]);
+        }
+
+        $user = User::where('login_token', $request->token)->first();
+
+
+        $user->login_token = null;
+
+        $user->save();
+
+        return response()->json(['status' => true, 'message' => 'Başarıyla çıkış yapıldı']);
+    }
+}
