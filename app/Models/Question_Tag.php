1a103239c873a532fa25499cc793bfc613fabd25
AYDIN BULUT <ab@aydinbulut.com>
2017-12-30 06:06:45 +0300

    Create Question_Tags model

---
+++ app/Models/Question_Tag.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question_Tag extends Model
+{
+    protected $table = 'question_tag';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'tag'
+    ];
+
+    public function questions()
+    {
+        return $this->belongsToMany(Question::class, 'question_tag', 'question_id');
+    }
+
+}
